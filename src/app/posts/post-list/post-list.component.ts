d965facd69de44c90e619da6b2796d481dc33e3a
Cesare Guardino <guardino@users.noreply.github.com>
2020-12-05 11:06:08 +0000

    Add looping over all posts and fallback if no posts

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
     styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent {
+    //posts = [
+    //    { title: "First post", content: "This is the first post's content" },
+    //    { title: "Second post", content: "This is the second post's content" },
+    //    { title: "Third post", content: "This is the third post's content" }
+    //];
+    posts = [];
 }
