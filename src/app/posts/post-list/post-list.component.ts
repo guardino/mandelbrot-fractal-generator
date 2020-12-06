e774127641c91bfff3f213d597f79f2d6ead4293
Cesare Guardino <guardino@users.noreply.github.com>
2020-12-06 11:10:18 +0000

    Use subscription to check for new posts, instead of direct access to the posts list in the PostsService

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
@@ -8,17 +9,26 @@ import { PostsService } from '../posts.service';
     templateUrl: './post-list.component.html',
     styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
-    //posts = [
-    //    { title: "First post", content: "This is the first post's content" },
-    //    { title: "Second post", content: "This is the second post's content" },
-    //    { title: "Third post", content: "This is the third post's content" }
-    //];
-    posts: Post[] = [];
+export class PostListComponent implements OnInit, OnDestroy {
+  //posts = [
+  //    { title: "First post", content: "This is the first post's content" },
+  //    { title: "Second post", content: "This is the second post's content" },
+  //    { title: "Third post", content: "This is the third post's content" }
+  //];
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
 
   ngOnInit() {
     this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
   }
 }
