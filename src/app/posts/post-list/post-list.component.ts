4a772dda7d062134b0266e0c056be7ffacdec3f5
Cesare Guardino <guardino@users.noreply.github.com>
2020-12-06 10:49:24 +0000

    Add PostsService to replace event binding/emitting properties

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,17 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
     selector: 'app-post-list',
     templateUrl: './post-list.component.html',
     styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit {
     //posts = [
     //    { title: "First post", content: "This is the first post's content" },
     //    { title: "Second post", content: "This is the second post's content" },
     //    { title: "Third post", content: "This is the third post's content" }
     //];
-    @Input() posts: Post[] = [];
+    posts: Post[] = [];
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+  }
 }
