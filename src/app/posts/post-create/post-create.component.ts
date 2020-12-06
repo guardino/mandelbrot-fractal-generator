3c9aabd5980fd2e5290c0a4df069db96079314e6
Cesare Guardino <guardino@users.noreply.github.com>
2020-12-06 22:54:29 +0000

    Use 2-level indentation

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -4,22 +4,22 @@ import { NgForm } from '@angular/forms';
 import { PostsService } from '../posts.service';
 
 @Component({
-    selector: 'app-post-create',
-    templateUrl: './post-create.component.html',
-    styleUrls: ['./post-create.component.css']
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-    enteredTitle = '';
-    enteredContent = '';
+  enteredTitle = '';
+  enteredContent = '';
 
-    onAddPost(form: NgForm) {
-        if (form.invalid) {
-          return;
-        }
-
-        this.postsService.addPost(form.value.title, form.value.content);
-        form.resetForm();
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
     }
 
-    constructor(public postsService: PostsService) {}
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
+  }
+
+  constructor(public postsService: PostsService) {}
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -5,9 +5,9 @@ import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
 
 @Component({
-    selector: 'app-post-list',
-    templateUrl: './post-list.component.html',
-    styleUrls: ['./post-list.component.css']
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit, OnDestroy {
   //posts = [
