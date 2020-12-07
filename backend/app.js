305343423ab158f0cf725d7c957157bd02216762
Cesare Guardino <guardino@users.noreply.github.com>
2020-12-07 23:05:16 +0000

    Get posts from server (also add CORS)

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,36 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('First middleware');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Header",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "ufheucsn3883",
+      title: "First server-side post",
+      content: "This is coming from the server"
+    },
+    {
+      id: "sd87n3widwl2",
+      title: "Second server-side post",
+      content: "This is coming from the server!"
+    }
+  ];
+
+  res.status(200).json({
+    message: 'Posts fetched succesfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
@@ -28,7 +29,8 @@ import { HeaderComponent } from './header/header.component';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
