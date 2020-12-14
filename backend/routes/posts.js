34d32d855522b16d6bbc77917a2a2885412bddee
Cesare Guardino <guardino@users.noreply.github.com>
2020-12-14 23:29:40 +0000

    Add file upload support

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const multer = require('multer');
 
 const router = express.Router();
 const Post = require('../models/post');
 
-router.post("", (req, res, next) => {
+const MIME_TYPE_MAP = {
+  'image/png' : 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg' : 'jpg'
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mime type");
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "backend/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + ext);
+  }
+});
+
+router.post("", multer({storage: storage}).single("image"), (req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -39,10 +39,18 @@ export class PostCreateComponent implements OnInit {
 
     this.isLoading = true;
     if (this.mode === "create") {
-      this.postsService.addPost(this.form.value.title, this.form.value.content);
+      this.postsService.addPost(
+        this.form.value.title,
+        this.form.value.content,
+        this.form.value.image
+      );
     }
     else {
-      this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
+      this.postsService.updatePost(
+        this.postId,
+        this.form.value.title,
+        this.form.value.content
+      );
     }
     this.form.reset();
   }
