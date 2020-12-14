b9431f51bd6f13e0ffce5b48d08d89ba3f3dbf90
Cesare Guardino <guardino@users.noreply.github.com>
2020-12-14 01:30:51 +0000

    Add progress spinner

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -14,6 +14,7 @@ export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
   post: Post;
+  isLoading = false;
   private mode = 'create';
   private postId: string;
 
@@ -22,6 +23,7 @@ export class PostCreateComponent implements OnInit {
       return;
     }
 
+    this.isLoading = true;
     if (this.mode === "create") {
       this.postsService.addPost(form.value.title, form.value.content);
     }
@@ -39,7 +41,9 @@ export class PostCreateComponent implements OnInit {
       {
         this.mode = 'edit';
         this.postId = paramMap.get('postId');
+        this.isLoading = true;
         this.postsService.getPost(this.postId).subscribe(postData => {
+          this.isLoading = false;
           this.post = { id: postData._id, title: postData.title, content: postData.content }
         });
       }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -16,14 +16,17 @@ export class PostListComponent implements OnInit, OnDestroy {
   //    { title: "Third post", content: "This is the third post's content" }
   //];
   posts: Post[] = [];
+  isLoading = false;
   private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
 
   ngOnInit() {
+    this.isLoading = true;
     this.postsService.getPosts();
     this.postsSub = this.postsService.getPostUpdateListener()
       .subscribe((posts: Post[]) => {
+        this.isLoading = false;
         this.posts = posts;
       });
   }
