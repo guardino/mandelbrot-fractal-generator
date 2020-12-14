13abf56252a03bf5b3f45e3c8d1f4b86d7694140
Cesare Guardino <guardino@users.noreply.github.com>
2020-12-14 17:38:11 +0000

    Add image controls to store image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -19,6 +19,14 @@ export class PostCreateComponent implements OnInit {
   private mode = 'create';
   private postId: string;
 
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
+    console.log(file);
+    console.log(this.form);
+  }
+
   onSavePost() {
     if (this.form.invalid) {
       return;
@@ -42,6 +50,9 @@ export class PostCreateComponent implements OnInit {
         validators: [Validators.required, Validators.minLength(3)]
       }),
       content: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      image: new FormControl(null, {
         validators: [Validators.required]
       })
     });
