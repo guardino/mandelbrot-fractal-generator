54e67f61a32bc6557d3de52586a9864d079a355c
Cesare Guardino <guardino@users.noreply.github.com>
2020-12-17 22:57:30 +0000

    Add init login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
   { path: 'create', component: PostCreateComponent },
-  { path: 'edit/:postId', component: PostCreateComponent }
+  { path: 'edit/:postId', component: PostCreateComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
