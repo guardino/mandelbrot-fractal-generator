8e817a355883ba141d1578ab08749cfd116b7978
Cesare Guardino <guardino@users.noreply.github.com>
2020-12-21 23:45:26 +0000

    Handling login form input

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   templateUrl: './login.component.html',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
   isLoading = false;
+
+  onLogin(form: NgForm) {
+    console.log(form.value);
+  }
 }
 
 
