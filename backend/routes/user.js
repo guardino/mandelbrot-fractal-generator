06ed4cc95fe4b5b631ebecea53264af9396ab11c
Cesare Guardino <guardino@users.noreply.github.com>
2020-12-22 08:40:07 +0000

    Implement SPA auth - create token on backend and send to client; also add jsonwebtoken package

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -24,6 +25,39 @@ router.post("/signup", (req, res, next) => {
             error: err
           });
         });
+    });
+});
+
+router.post("/login", (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      const token = jwt.sign({ email: fetchedUser.email, userId: fetchedUser._id },
+        'secret_this_should_be_longer',
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({
+        token: token
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Auth failed"
+      });
     });
 });
 
