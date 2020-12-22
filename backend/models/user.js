4943f62f113dae730f3e4229873b828e47074be5
Cesare Guardino <guardino@users.noreply.github.com>
2020-12-22 00:49:15 +0000

    Correct type in User model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uniqueValidator = required('mongoose-unique-validator');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true},
