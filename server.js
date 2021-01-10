fb135ce5fc59f77d66bcbae5ca5d3de80359d764
Cesare Guardino <guardino@users.noreply.github.com>
2021-01-10 21:14:18 +0000

    Reduce diffs from Max's version in main server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const http = require('http');
+const app = require("./backend/app");
 //const debug = require("debug")("node-angular");
-const app = require('./backend/app');
+const http = require("http");
 
 const port = process.env.PORT || 3000;
+app.set("port", port);
 
-app.set('port', port);
 const server = http.createServer(app);
-
 server.listen(port);
