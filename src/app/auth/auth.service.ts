d0b6e0233d97396c21de24fc2e2d1cee4170e81c
Cesare Guardino <guardino@users.noreply.github.com>
2021-01-10 08:13:34 +0000

    Improve the UI to reflect auth status

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 import { AuthData } from './auth-data.model';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
   private token: string;
+  private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient) {}
 
   getToken() {
     return this.token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
   }
 
   createUser(email: string, password: string) {
@@ -27,6 +33,7 @@ export class AuthService {
       .subscribe(response => {
         const token = response.token;
         this.token = token;
+        this.authStatusListener.next(true);
       });
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  userIsAuthenticated = false;
+
+  private authListenerSubs: Subscription;
+
+  constructor(public authService: AuthService) {}
+
+  ngOnInit() {
+    this.authListenerSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe(isAuthenticated => {
+        this.userIsAuthenticated = isAuthenticated;
+      });
+  }
+
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
+  }
 }
 
