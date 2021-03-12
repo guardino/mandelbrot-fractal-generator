e437fb6befd8eba8c945f9aa13431c3a89e39cd8
Cesare Guardino <guardino@users.noreply.github.com>
2021-03-12 21:04:36 +0000

    Increase default posts per page to 10 and allow up to 20 posts per page

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -20,9 +20,9 @@ export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   isLoading = false;
   totalPosts = 0;
-  postsPerPage = 5;
+  postsPerPage = 10;
   currentPage = 1;
-  pageSizeOptions = [1, 2, 5, 10];
+  pageSizeOptions = [1, 2, 5, 10, 20];
   userIsAuthenticated = false;
   userId: string;
   showUserOnlyPosts: boolean = false;
