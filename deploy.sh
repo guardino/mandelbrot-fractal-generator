043e2bf3673d2e8ae6b5ff8604aff68aa75ee724
Cesare Guardino <guardino@users.noreply.github.com>
2021-03-25 11:45:10 +0000

    Rename all deployment scripts for clarity

--- deploy.sh
+++ deploy.sh
@@ -13,5 +13,5 @@ ng build --prod
 tar cvf backend.tar backend && gzip backend.tar
 
 scp -i $SSH_KEY backend.tar.gz $USER@$SERVER:~/
-scp -i $SSH_KEY start.sh $USER@$SERVER:~/
-ssh -i $SSH_KEY $USER@$SERVER "~/start.sh &"
+scp -i $SSH_KEY deploy_start.sh $USER@$SERVER:~/
+ssh -i $SSH_KEY $USER@$SERVER "~/deploy_start.sh &"
