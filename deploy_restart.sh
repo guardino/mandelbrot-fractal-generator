771be084100d939bfa5ab7b7c9be15ec47b7890a
Cesare Guardino <guardino@users.noreply.github.com>
2021-04-20 22:06:03 +0100

    Correct restart script for Raspberry Pi Zero

--- deploy_restart.sh
+++ deploy_restart.sh
@@ -2,4 +2,4 @@
 
 cd ~/backend
 export PATH=~/apps/node-v14.15.1-linux-armv6l/bin:$PATH
-MONGO_ATLAS_PW=kbGRjDotp9D6mHN3 JWT_KEY=secret_this_should_be_longer node server.js &
+MONGO_ATLAS_PW=MONGO_ATLAS_PW JWT_KEY=JWT_KEY node server.js &
